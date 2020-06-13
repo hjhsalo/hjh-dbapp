b03130c6a8c9d245945409eac2bd07728a6605fc
Harri Hirvonsalo <hjhsalo@gmail.com>
2020-06-13 04:29:00 +0300

    Add test case boilerplate code
    Signed-off-by: Harri Hirvonsalo <hjhsalo@gmail.com>

--- src/test/java/com/hjhsalo/dbapp/AppTest.java
+++ src/test/java/com/hjhsalo/dbapp/AppTest.java
@@ -1,20 +1,56 @@
+/*
+ *     Copyright (c) 2020 Harri Hirvonsalo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hjhsalo.dbapp;
 
-import static org.junit.Assert.assertTrue;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import picocli.CommandLine;
+
+public class AppTest {
+    final PrintStream originalOut = System.out;
+    final PrintStream originalErr = System.err;
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    final ByteArrayOutputStream err = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        out.reset();
+        err.reset();
+        System.setOut(new PrintStream(out));
+        System.setErr(new PrintStream(err));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test() {
+        String[] args = "".split(" ");
+        new CommandLine(new DbApp()).execute(args);
+        // assertEquals("", out.toString());
+        // assertEquals("", err.toString());
+        assertEquals("", "");
     }
 }
